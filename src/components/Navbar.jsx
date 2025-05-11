a8a1cf4bb6eeb980d918ac6cef3fc7e266340b1a
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-12 00:50:07 +0500

    naav-2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,35 @@
+"use client"
+
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter, usePathname } from 'next/navigation'
+
+const CustomLink = ({href, title, className=""}) => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const isActive = pathname === href;
+
+    console.log(router)
+    return (
+        <Link href={href} className={`${className} relative group`}>
+                {title}
+
+                <span className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${isActive ? 'w-full' : ''}`}>&nbsp;</span>
+        </Link>
+    )
+}
 
 const Navbar = () => {
   return (
     <header className='font-montserrat w-full px-32 py-8 font-medium flex items-center justify-between'>
         <nav>
-            <Link href={'/'}>Home</Link>
-            <Link href={'/about'}>About</Link>
-            <Link href={'/projects'}>Projects</Link>
-            <Link href={'/articles'}>Articles</Link>
+            <CustomLink href={'/'} title={'Home'} className='mr-4' />
+            <CustomLink href={'/about'} title={'About'} className='mx-4' />
+            <CustomLink href={'/projects'} title={'Projects'} className='mx-4' />
+            <CustomLink href={'/articles'} title={'Articles'} className='ml-4' />
         </nav>
-
-        <Logo />
 
         <nav>
             <Link href={'/'} target='_blank'>T</Link>
@@ -22,6 +39,10 @@ const Navbar = () => {
             <Link href={'/'} target='_blank'>T</Link>
             <Link href={'/'} target='_blank'>T</Link>
         </nav>
+
+        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            <Logo />
+        </div>
     </header>
   )
 }
