c8fb776edaef02c90c9f31f617bfdd36cacb3412
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-11 23:55:49 +0500

    font set

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const {fontFamily} = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        // fontFamily: {
+        //     mont: ['var(--font-montserrat)', ...fontFamily.sans],
+        // },
+    },
+  },
+  plugins: [],
+}
