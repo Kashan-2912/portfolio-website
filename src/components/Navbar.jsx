16ee3b7bf337e1f36e1da93b95df78884e769e60
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-17 11:50:17 +0500

    navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,86 @@
-"use client"
-import Link from 'next/link'
-import React from 'react'
-import Logo from './Logo'
-import { useRouter, usePathname } from 'next/navigation'
+"use client";
+import Link from "next/link";
+import React from "react";
+import Logo from "./Logo";
+import { useRouter, usePathname } from "next/navigation";
+import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon } from "./Icons";
+import { motion } from "framer-motion";
 
-const CustomLink = ({href, title, className=""}) => {
-    const router = useRouter();
-    const pathname = usePathname();
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+  const pathname = usePathname();
 
-    const isActive = pathname === href;
+  const isActive = pathname === href;
 
-    console.log(router)
-    return (
-        <Link href={href} className={`${className} relative group`}>
-                {title}
+  console.log(router);
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
 
-                <span className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${isActive ? 'w-full' : ''}`}>&nbsp;</span>
-        </Link>
-    )
-}
+      <span
+        className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          isActive ? "w-full" : ""
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
-    <header className='font-montserrat w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <CustomLink href={'/'} title={'Home'} className='mr-4' />
-            <CustomLink href={'/about'} title={'About'} className='mx-4' />
-            <CustomLink href={'/projects'} title={'Projects'} className='mx-4' />
-            <CustomLink href={'/articles'} title={'Articles'} className='ml-4' />
-        </nav>
+    <header className="font-montserrat w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <CustomLink href={"/"} title={"Home"} className="mr-4" />
+        <CustomLink href={"/about"} title={"About"} className="mx-4" />
+        <CustomLink href={"/projects"} title={"Projects"} className="mx-4" />
+        <CustomLink href={"/articles"} title={"Articles"} className="ml-4" />
+      </nav>
 
-        <nav>
-            <Link href={'/'} target='_blank'>T</Link>
-            <Link href={'/'} target='_blank'>T</Link>
-            <Link href={'/'} target='_blank'>T</Link>
-            <Link href={'/'} target='_blank'>T</Link>
-            <Link href={'/'} target='_blank'>T</Link>
-            <Link href={'/'} target='_blank'>T</Link>
-        </nav>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a href="google.com" target="_blank"
+        whileHover={{y: -2}}
+        whileTap={{scale: 0.9}}
+        className="w-6 mx-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a href="google.com" target="_blank"
+        whileHover={{y: -2}}
+        whileTap={{scale: 0.9}}
+        className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a href="google.com" target="_blank"
+        whileHover={{y: -2}}
+        whileTap={{scale: 0.9}}
+        className="w-6 mx-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="google.com" target="_blank"
+        whileHover={{y: -2}}
+        whileTap={{scale: 0.9}}
+        className="w-6 mx-3"
+        >
+          <PinterestIcon />
+        </motion.a>
+        <motion.a href="google.com" target="_blank"
+        whileHover={{y: -2}}
+        whileTap={{scale: 0.9}}
+        className="w-6 mx-3"
+        >
+          <DribbbleIcon />
+        </motion.a>
+      </nav>
 
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-            <Logo />
-        </div>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
