bcea1c007c73353e009f7c794ce0b2df137417c6
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-19 21:38:23 +0500

    minor fix  </>

--- src/app/page.js
+++ src/app/page.js
@@ -42,8 +42,8 @@ export default function Home() {
       </div> */}
 
       <div className="absolute right-8 bottom-8 inline-block w-24">
-        <span class="relative flex items-center justify-center w-10 h-10">
-          <span class="absolute inline-flex h-full w-full animate-ping hover:pause-animation rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative flex items-center justify-center w-10 h-10">
+          <span className="absolute inline-flex h-full w-full animate-ping hover:pause-animation rounded-full bg-sky-400 opacity-75"></span>
           <img alt="Kashan" src="/dev.svg" className="w-full h-auto" />
         </span>
       </div>
