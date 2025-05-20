c17c97fc93abe289a3b196144768864d06dc5ec7
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-20 19:45:53 +0500

    font montserrat globally lol forgot in beginning

---
+++ src/app/about/page.jsx
@@ -0,0 +1,53 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Head>
+        <title>Kashan | About Page</title>
+        <meta
+          name="description"
+          content="Is your website struggling to load quickly? You've come to the right profile. When you connect with Muhammad Kashan Ashraf, you're not just hiring a developer or a designer—you're gaining a partner committed to bringing your vision to life. With a rich blend of technical skills and creative design expertise, I ensure that every project is functional, visually stunning, and user-friendly. My holistic approach provides comprehensive solutions tailored to your specific needs. Clear communication, timely delivery, and exceptional quality are the hallmarks of my work. Partnering with me means your project is in capable hands, driving your business forward with innovative and efficient digital solutions."
+        />
+      </Head>
+
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className={"pt-16"}>
+          <AnimatedText text="Passion Fuels Purpose!" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark opacity-75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                passion for creating beautiful, functional, and user-centered
+                digital experiences. With 4 years of experience in the field. I
+                am always looking for new and innovative ways to bring my
+                clients' visions to life.
+              </p>
+              <p className="font-medium my-4">
+                {" "}
+                I believe that design is about more than just making things look
+                pretty – it's about solving problems and creating intuitive,
+                enjoyable experiences for users.
+              </p>
+              <p className="font-medium">
+                Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default page;
