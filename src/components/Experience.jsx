8344fd75799f61e356450925acbfc8fa6de0bee8
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-23 16:51:49 +0500

    Li Icon on scroll Progress

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,10 +2,12 @@
 
 import { motion, useScroll } from "framer-motion";
 import React, { useRef } from "react";
+import LiIcon from "./LiIcon";
 
 const Details = ({ position, company, companyLink, time, address, work }) => {
   return (
     <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between">
+        <LiIcon />
       <div>
         <h3 className="capitalize text-2xl font-bold">
           {position}&nbsp;
