fbd143fb2b9937ed7a3486992142d341ab298f80
Muhammad Kashan Ashraf <mkashan2912@gmail.com>
2025-05-23 18:30:25 +0500

    Education section completed

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -17,9 +17,7 @@ const Details = ({ type, time, place, info }) => {
         whileInView={{ y: 0 }}
         transition={{ duration: 0.5, type: "spring" }}
       >
-        <h3 className="capitalize text-2xl font-bold">
-          {type}
-        </h3>
+        <h3 className="capitalize text-2xl font-bold">{type}</h3>
         <span className="capitalize font-medium text-dark/75">
           {time} | {place}
         </span>
@@ -39,9 +37,7 @@ const Education = () => {
 
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center">
-        Experience
-      </h2>
+      <h2 className="font-bold text-8xl mb-32 w-full text-center">Education</h2>
 
       <div ref={ref} className="w-[75%] mx-auto relative">
         <motion.div
@@ -52,34 +48,75 @@ const Education = () => {
         <ul className="w-full flex flex-col items-start justify-between ml-4">
           <Details
             type="Bachelor Of Science In Computer Science"
-            time="2016-2020"
-            place="Massachusetts Institute Of Technology (MIT)"
-            info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
-Intelligence."
+            time="2022-2026"
+            place={
+              <>
+                Islamabad - Pakistan | <strong>CGPA: 3.50/4.00</strong>
+              </>
+            }
+            info={
+              <>
+                Relevant courses till now include Introduction to ICT,
+                Programming Fundamentals, Data Structures and Algorithms, Object
+                Oriented Programming, Database Systems, Computer Networks,
+                Theory of Automata, Design and Analysis of Algorithms,
+                Principles of Operating Systems, Web Technologies, Information
+                Security, Artificial Intelligence, Software Engineering
+                Concepts, Mobile Application Development, Computer Organization
+                & Assembly Language, Machine Learning and Numerical
+                Computations.
+                <br />
+                <br />
+                Participated in COMPETE'24, a university level competitive Web
+                Development event, GDSC Bootcamps on Build with AI.
+              </>
+            }
           />
 
           <Details
-            type="Bachelor Of Science In Computer Science"
-            time="2016-2020"
-            place="Massachusetts Institute Of Technology (MIT)"
-            info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
-Intelligence."
+            type="Online Coursework"
+            time="2022-Present"
+            place="Udemy - Coursera - Edx"
+            info={<>
+                Completed a variety of online courses covering key technical and creative skills. <strong>Notable courses include:</strong>
+                <br />
+                <br />
+                1. Web Development Bootcamp (Angela Yu, 2025)<br />
+                2. Mobile App Development (Angela Yu, 2025)<br />
+                3. Programming for Everybody (University of Michigan)<br />
+                4. Python for Everybody, DSA in Python<br />
+                5. Graphic Design Fundamentals.
+            </>}
           />
 
           <Details
-            type="Bachelor Of Science In Computer Science"
-            time="2016-2020"
-            place="Massachusetts Institute Of Technology (MIT)"
-            info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
-Intelligence."
+            type="Pre-Engineering | Intermediate (HSSC)"
+            time="2020-2022"
+            place="Islamabad College for Boys, G-6/3, Islamabad"
+            info={
+              <>
+                Secured <strong>93%</strong> marks in the Federal Board
+                examinations, demonstrating strong academic performance in core
+                subjects like Mathematics, Physics, and Chemistry. Developed a
+                solid foundation for further studies in engineering and
+                technology.
+              </>
+            }
           />
 
           <Details
-            type="Bachelor Of Science In Computer Science"
-            time="2016-2020"
-            place="Massachusetts Institute Of Technology (MIT)"
-            info="Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Artificial 
-Intelligence."
+            type="Secondary School | SSC"
+            time="2018-2020"
+            place="Islamabad Model School for Boys, G-6/4."
+            info={
+              <>
+                Achieved <strong>96%</strong> marks in the Federal Board SSC
+                examinations, with exceptional performance in core science
+                subjects like Physics and Mathematics. Built a strong academic
+                foundation and <strong>ranked number 1</strong> in the
+                institution.
+              </>
+            }
           />
         </ul>
       </div>
